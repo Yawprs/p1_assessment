ef3264546e560ea939b9ec6ea89a9508ea030041
Drake Tyksinski <drake.tyksinski@live.com>
2021-01-06 11:59:23 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let container = document.getElementById("container")
+
+let output = document.getElementById("output")
+
+let input = document.getElementById("input")
+
+let minus = document.getElementById("minus")
+
+let plus = document.getElementById("plus")
+
