3e0afed11485b096d8ff19944975ce9346678f5b
Drake Tyksinski <drake.tyksinski@live.com>
2021-01-06 12:32:21 -0800

    changed things

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let container = document.getElementById("container")
-
 let output = document.getElementById("output")
 
 let input = document.getElementById("input")
@@ -8,3 +6,16 @@ let minus = document.getElementById("minus")
 
 let plus = document.getElementById("plus")
 
+function addition() {
+    output.innerText = (input.value + output.value)
+    console.log("u clicked")
+}
+
+plus.addEventListener("click", addition)
+
+
+function subtraction() {
+    output.innerText = (input.value - output.value)
+}
+
+minus.addEventListener("click", subtraction)
